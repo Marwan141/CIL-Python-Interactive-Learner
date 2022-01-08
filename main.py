00fe5cd6c2875ccf965a373c271edaa13c818683
Marwan Yassini <yassinimarwan6@gmail.com>
2022-01-08 22:06:08 +0000

    more bugs :D

--- main.py
+++ main.py
@@ -13,26 +13,28 @@ checknum = 0
 #Save file function 
 
 #Introduction, input name and check
-print("Welcome to Marwan's CIL Python Tutorial.")
-sleep(2)
-pname = input("To start, Please input your name: ")
+def intro():
+    print("Welcome to Marwan's CIL Python Tutorial.")
+    sleep(2)
+    pname = input("To start, Please input your name: ")
 
-name_choice = input("Hello " + pname + ", is that correct?")
+    name_choice = input("Hello " + pname + ", is that correct?")
 
-while (name_choice != "Yes"):
-    pname = input("Please input your name again: ")
-    name_choice = input("Hello " + pname + "is that correct?")
+    while (name_choice != "Yes"):
+        pname = input("Please input your name again: ")
+        name_choice = input("Hello " + pname + "is that correct?")
 
 
+    print("Well " + pname + ", nice to meet you, we will first start with the basics in Python or any programming language that is.")
+    sleep(4)
+    print("Printing!")
+    sleep(2)
 
-print("Well " + pname + ", nice to meet you, we will first start with the basics in Python or any programming language that is.")
-sleep(4)
-print("Printing!")
-sleep(2)
 
 #First stage, printing
-while printing == False:
-    print("In Python, we use the print() function to display stuff on the user's screen. For example, print('Hello World') will display Hello World on your screen. Try it!")
+def printing_stage():
+    while printing == False:
+        print("In Python, we use the print() function to display stuff on the user's screen. For example, print('Hello World') will display Hello World on your screen. Try it!")
 
     first_print = input()
     try:
@@ -50,22 +52,56 @@ while printing == False:
         print("I see you printed something else... it's fine, you do you.")
         printing == True
 
-sleep(3)
 
 #Second stage, variables
-while vari == False:
-    print("Another important concept that is to be learnt in Python is handling variables.")
-    sleep(4)
-    print("To initialize a variable, in python we just use this command: (variable_name = (contents of variable))")
-    sleep(4)
-    print("This might be a bit confusing at first but an example should help, age = 16")
-    sleep(4)
-    print("As we can see now the variable, which is basically a box holding a number in this case, holds the number 16 and we can call back on this whenever we want.")
-    sleep(4)
-    print("Try it yourself! Make a variable.")
-    first_vari = input()
-    try:
-        exec(first_print)
-        exec("checknum = 1")
-    except:
-        pass
+def vari_stage():
+    while vari == False:
+        
+        def variable_tutorial():
+            print("To initialize a variable, in python we just use this command: (variable_name = (contents of variable))")
+            sleep(4)
+            print("This might be a bit confusing at first but an example should help, age = 16")
+            sleep(4)
+            print("As we can see now the variable, which is basically a box holding a number in this case, holds the number 16 and we can call back on this whenever we want.")
+            sleep(4)
+            print("Try it yourself! Make a variable.")
+            first_vari = input()
+            try:
+                exec(first_vari)
+                vari = True
+                return first_vari
+            
+            except:
+                reminder = input("Something is wrong with your code. Would you like to be reminded of how to intialize a variable? If so press Y, otherwise press any other button to try again.")
+                if reminder == "Y":
+                    variable_tutorial()
+                else:
+                    execute_var()
+        def execute_var():
+            print("Make a variable.")
+            first_vari = input()
+            try:
+                exec(first_vari)
+            except:
+                reminder = input("Something is wrong with your code. Would you like to be reminded of how to intialize a variable? If so press Y, otherwise press any other button to try again.")
+                while reminder != "Y":
+                    execute_var()
+            
+        print("Another important concept that is to be learnt in Python is handling variables.")
+        sleep(4)
+        variable_tutorial()
+        print("Great job! Now we can try and use the print() function we learnt earlier, to print this variable you just made! Just call back the variable inside the print function. I'll let you figure this one out on your own (;")
+        print_vari = input()
+        try:
+            exec(print_vari)
+        except:
+            print_reminder = input("Uh Oh, something went wrong with your code. Would you like a hint?")
+
+#printing_stage()
+#save_choice = input("Would you like to continue or save? C for continue or any other button to save and stop.")   
+#if save_choice != "C":
+#    pass  #save function to be added here
+#else:
+#    pass
+
+vari_stage()
